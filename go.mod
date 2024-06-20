290345190f79b942e7b9fefd243b71a10c0bec81
steve kieu <msh.computing@gmail.com>
2024-06-20 22:02:53 +1000

    Allow to pass static dir and secret

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module static-webserver-with-jwt
 
 go 1.22.2
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+require github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/v4"
 )
-
-// Define a secret key used to sign the JWTs (should be stored securely in production)
-var jwtSecret = []byte("secret-key-for-jwt")
 
 // Struct for JWT claims
 type Claims struct {
@@ -19,7 +18,7 @@ type Claims struct {
 }
 
 // Middleware function to authenticate JWT
-func authenticate(next http.Handler) http.Handler {
+func authenticate(next http.Handler, jwtSecret []byte) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Get the token from the Authorization header
 		authHeader := r.Header.Get("Authorization")
@@ -59,15 +58,39 @@ func authenticate(next http.Handler) http.Handler {
 }
 
 func main() {
-	// Create a file server for the "static" directory
-	fs := http.FileServer(http.Dir("./static"))
+	// Define flags for static directory path, JWT secret environment variable, and server port
+	var staticDir string
+	var jwtSecret string
+	var port string
+
+	flag.StringVar(&staticDir, "static-dir", "./static", "Path to the static directory")
+	flag.StringVar(&jwtSecret, "jwt-secret", "", "JWT secret key (environment variable)")
+	flag.StringVar(&port, "port", "8080", "Port for the server to listen on")
+
+	flag.Parse()
+
+	// Validate the static directory path
+	if _, err := os.Stat(staticDir); os.IsNotExist(err) {
+		log.Fatalf("Static directory '%s' does not exist", staticDir)
+	}
+
+	// Validate JWT secret
+	if jwtSecret == "" {
+		log.Fatal("JWT secret is not provided")
+	}
+
+	// Convert JWT secret to []byte
+	jwtSecretBytes := []byte(jwtSecret)
+
+	// Create a file server for the static directory
+	fs := http.FileServer(http.Dir(staticDir))
 
 	// Handle requests to serve static files with authentication middleware
-	http.Handle("/", authenticate(fs))
+	http.Handle("/", authenticate(fs, jwtSecretBytes))
 
 	// Start the server
-	port := ":8080"
-	fmt.Printf("Server listening on port %s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	addr := ":" + port
+	fmt.Printf("Server listening on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
